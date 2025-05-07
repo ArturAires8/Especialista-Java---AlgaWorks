ea8470f7a84e3084adbb3a8f57c68022b4270169
Artur Aires <166089713+ArturAires8@users.noreply.github.com>
2025-05-06 22:13:35 -0300

    Add files via upload

---
+++ 02.11 Operadoresde incremento e decremento/src/OperadoresIncremento.java
@@ -0,0 +1,22 @@
+public class OperadoresIncremento {
+
+    public static void main(String[] args) {
+        int totalDownloads = 10;
+
+        // totalDownloads = totalDownloads + 1;
+        // totalDownloads += 1;
+
+        // totalDownloads++;
+
+        // int novoTotalDownloads = totalDownloads++;
+        int novoTotalDownloads = ++totalDownloads;
+
+        System.out.println("Total de downloads: " + totalDownloads);
+        System.out.println("Novo total de downloads: " + novoTotalDownloads);
+    }
+
+}
+
+
+
+
